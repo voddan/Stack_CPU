1a5b3cf8532543362230be4ed3f6b6173fa028b8
voddan <dgvodopyan@gmail.com>
2014-04-22 19:47:36 +0400

    created Com_Arg, Com_Non; wrote additional classes Reg, Code

--- Compiler/command.hpp
+++ Compiler/command.hpp
@@ -11,23 +11,56 @@
 #include <sstream>
 #include <typeinfo>
 
+#include <assert.h>
+
 #include "../utils.hpp"
 
 using namespace std;
 using namespace utils;
 
-namespace command {
+namespace command { // Code; Reg
+	
+#define CODE_SIZE 64
+struct Code : Printable{
+	Code(int x) : val((assert(0 <= x && x < CODE_SIZE), x)) {}
+	const int val;
+	virtual string to_string() const { 
+		ostringstream str;
+		str << std::hex << val;
+		return str.str();
+	}
+};
+
+#define REG_SIZE 8
+struct Reg : Printable{
+	Reg(int x) : val((assert(0 <= x && x < REG_SIZE), x)) {}
+	const int val;
+	virtual string to_string() const { 
+		ostringstream str;
+		str << std::hex << val;
+		return str.str();
+	}
+};
+	
+}
+
+namespace command { // Command; Command_list
 	
 class Command : public Printable{
 	public:
-		Command(string name) : _name(name) {}
+		Command(string name, int code) 
+			: _name(name), _code(Code(code)) {}
 		virtual ~Command() {}
-		//----------------
-		string name() {return _name;}
+		
+		string 	name() const {return _name;}
+		int 	code() const {return _code.val;}
+		
 		virtual string to_string() const {return "Command(" + _name + ")";}
+		//----------------
 
 	private:
 		const string _name;
+		const Code _code;
 };
 
 class Command_list : public list <Command*>, public Printable {
@@ -53,4 +86,61 @@ class Command_list : public list <Command*>, public Printable {
 };
 
 }
+
+namespace command { // Com_Arg; Com_Non
+	
+class Com_Arg : public Command{
+	public:
+		Com_Arg(string name, int code, int reg, int arg)
+			: Command(name, code), _reg(Reg(reg)), _arg(arg) {}
+		virtual ~Com_Arg() {}
+		
+		int 	reg() const {return _reg.val;}
+		int 	arg() const {return _arg;}
+		
+		virtual string to_string() const {
+			ostringstream str;
+			str << "Com_Arg(" << name() << ": "; 
+			str << _reg.to_string() << ", "; 
+			str << _arg		<< ")";
+			return str.str();
+		}
+		
+		//----------------
+
+	private:
+		const Reg _reg;
+		const int _arg;
+};
+
+class Com_Non : public Command{
+	public:
+		Com_Non(string name, int code, int reg, int reg_1, int reg_2)
+			: Command(name, code), 
+			_reg(Reg(reg)), _reg_1(Reg(reg_1)), _reg_2(Reg(reg_2))  {}
+		virtual ~Com_Non() {}
+		
+		int 	reg() const {return _reg.val;}
+		int 	reg_1() const {return _reg_1.val;}
+		int 	reg_2() const {return _reg_2.val;}
+		
+		virtual string to_string() const {
+			ostringstream str;
+			str << "Com_Non(" << name() << ": "; 
+			str << _reg.to_string() << " ~ "; 
+			str << _reg_1.to_string() << ", "; 
+			str << _reg_2.to_string() << ")"; 
+			return str.str();
+		}
+		
+		//----------------
+
+	private:
+		const Reg _reg;
+		const Reg _reg_1;
+		const Reg _reg_2;
+};	
+
+};
+
 #endif // _COMMAND_HPP_
