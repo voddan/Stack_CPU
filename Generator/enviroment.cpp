c2bcf534da50ae8be2a7fdc97f2cf9674873bc6b
voddan <dgvodopyan@gmail.com>
2014-04-25 22:28:13 +0400

    started generator

---
+++ Generator/enviroment.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+//-----------------------------------------------------
+class Obj {
+	public:
+		Obj(int x): value(x) {}
+		static int state;
+		
+		void dump();
+	private:
+		int value;
+};
+
+int Obj::state = 0;
+
+Obj* obj;
+Obj head(0);
+//-----------------------------------------------------
+extern "C"
+void Obj::dump() {
+	printf("tdump_obj\n");
+	printf("state: %d\n", Obj::state);
+	printf("value: %d\n", obj->value);
+}
+
+extern "C"
+void init_obj() {
+	printf("init_obj\n");
+	obj = new Obj(239);
+	Obj::state = 45;
+}
+
+extern "C"
+void del_obj() {
+	printf("del_obj\n");
+	delete obj;
+	Obj::state = -13;
+}
+
