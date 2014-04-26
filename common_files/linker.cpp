2401934479cceb5b08582a0959c56277390e1999
voddan <dgvodopyan@gmail.com>
2014-04-26 15:26:59 +0400

    picked up things into Linker

--- common_files/linker.cpp
+++ common_files/linker.cpp
@@ -6,6 +6,10 @@
 #include <string>
 #include <list>
 #include <sstream>
+#include <fstream>
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
 
 #include <assert.h>
 
@@ -18,10 +22,47 @@
 using namespace std;
 using namespace utils;
 
-namespace linker {
+namespace linker { // DATA
 
 int* Linker::_registers[] = {};
 
+Com_Arg::execute_func_t Linker::commands_arg[CODE_SIZE] = {};
+Com_Non::execute_func_t Linker::commands_non[CODE_SIZE] = {};
+
+}
+////////////////////////////////////////////////////////////////////////
+
+namespace linker { // Stack_CPU::
+	
+void Linker::load_registers(int arr[REG_SIZE]) {
+	for(int i = 0; i < REG_SIZE; i++) {
+		  _registers[i] = (arr + i);
+		*(_registers[i]) = 0;
+	}
+}
+
+bool Linker::load_instructions(ifstream& stream) {
+	char ch = 0;
+	
+	while( stream.read(&ch , 1) )
+		instructions.push_back(ch);
+		
+	return true;
+}
+
+void Linker::add_commands_arg(pair<Code, Com_Arg::execute_func_t> p) {
+	Linker::commands_arg[p.first.val] = p.second;
+}
+
+void Linker::add_commands_non(pair<Code, Com_Non::execute_func_t> p) {
+	Linker::commands_non[p.first.val] = p.second;
+}
+
+}
+////////////////////////////////////////////////////////////////////////
+
+namespace linker { // dump_...
+	
 void Linker::dump_registers_val(ostream& stream) {
 	stream << "#! dump of Linker::registers (values)" << endl;
 	for(int i = 0; i < REG_SIZE; i++) {
@@ -35,15 +76,44 @@ void Linker::dump_registers(ostream& stream) {
 		stream << "#! " << _registers[i] << endl;
 	}
 }
-
-
-void Linker::load_registers(int arr[REG_SIZE]) {
-	for(int i = 0; i < REG_SIZE; i++) {
-		  _registers[i] = (arr + i);
-		*(_registers[i]) = 0;
+	
+void Linker::dump_instructions(ostream& stream) {
+	const char names[16] = {
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'a', 'b', 'c', 'd', 'e', 'f' 
+	};
+	long count = 0;
+	
+	for( vector<char>::const_iterator iter = instructions.begin();
+			iter != instructions.end(); iter++) {
+		stream << names[(*iter >> 4) & 0x0f];
+		stream << names[*iter & 0x0f];
+		stream << " ";
+		
+		count++;
+		if(count % 16 == 0) stream << "\n";
 	}
 }
-
-
+	
+void Linker::dump_commands_arg(ostream& stream) {
+	stream << "#! dump of Linker.commands_arg\n";
+	assert(stream == cout);
+	for (int i = 0; i < CODE_SIZE; i++) {
+		stream << "#! [" << setw(3) << i << "] ";
+		printf("%p\n", commands_arg[i]);
+	}
+}
+	
+void Linker::dump_commands_non(ostream& stream) {
+	stream << "#! dump of Linker.commands_non\n";
+	assert(stream == cout);
+	for (int i = 0; i < CODE_SIZE; i++) {
+		stream << "#! [" << setw(3) << i << "] ";
+		printf("%p\n", commands_non[i]);
+	}
+}
+	
+	
+	
 }
 ////////////////////////////////////////////////////////////////////////
--- common_files/linker.hpp
+++ common_files/linker.hpp
@@ -1,5 +1,7 @@
 /* linker.hpp
  * 
+ * Things, which are needed by Command to work,
+ * but may changes because of Stack_CPU
  * */
  
 #ifndef _LINKER_HPP_
@@ -14,21 +16,47 @@
 #include <utils.hpp>
 #include <consts.hpp>
 
+#include <command.hpp>
+
 using namespace std;
 using namespace utils;
+using namespace command;
 
 namespace linker {
 
-struct Linker{ // POD
+struct Linker{
 	static void dump_registers(ostream& stream);
 	static void dump_registers_val(ostream& stream);
+	// dump stacks
+	void dump_commands_arg	(ostream& stream);
+	void dump_commands_non	(ostream& stream);
+	
+	void dump_instructions	(ostream& stream);
+	
 	//---------------------------
 	
 	static void load_registers(int arr[REG_SIZE]);
 	
+	static void add_commands_arg(pair<Code, Com_Arg::execute_func_t>);
+	static void add_commands_non(pair<Code, Com_Non::execute_func_t>);
+	
+	bool load_instructions(ifstream& stream);
+	
+	//---------------------------
+	// operations with stacks
+	// operations special registers
 	private:
 		static int* _registers[REG_SIZE];
+		
+		static Com_Arg::execute_func_t commands_arg[CODE_SIZE];
+		static Com_Non::execute_func_t commands_non[CODE_SIZE];
+		
+		static int* ip_register;
+		static int* ret_register;
+		
+		static vector<char> instructions;
 };
+
 
 }
 ////////////////////////////////////////////////////////////////////////
